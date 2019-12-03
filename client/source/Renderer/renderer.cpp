b8c1cd0c637f949a2df8f4aa759ced4bf6ebb654
gavroman <gavroman1998@gmail.com>
2019-12-03 15:49:06 +0300

    View changed to sfml realization

--- client/source/Renderer/renderer.cpp
+++ client/source/Renderer/renderer.cpp
@@ -3,7 +3,6 @@
 renderer::renderer(sf::RenderWindow* win) {
     window = win;
     view = window->getView();
-    view.zoom(1.0f);
 }
 
 int renderer::init(std::vector<sf::Texture*> cars_textures, sf::Texture* map_texture) {
@@ -17,35 +16,16 @@ int renderer::init(std::vector<sf::Texture*> cars_textures, sf::Texture* map_tex
     return 0;
 }
 
-
 int renderer::build_game_scene(game_render_data data) {
     window->clear();
-
-    sf::Vector2f center(data.players[0].position.x, data.players[0].position.y);
-
-    // {
-    //     float width = window->getSize().x;
-    //     float height = window->getSize().y;
-    //     map.setTextureRect(sf::IntRect(center.x - width / 2, center.y - height / 2, width, height));
-    //     window->draw(map);
-    //     for (size_t i = 0; i != players.size(); i++) {
-    //         players[i].setPosition(data.players[i].position.x - data.players[0].position.x + width / 2,
-    //                                data.players[i].position.y - data.players[0].position.y + height / 2);
-    //         players[i].setRotation(data.players[i].position.angle);
-    //         window->draw(players[i]);
-    //     }
-    // }
-    {
-        view.setCenter(center);
-        window->setView(view);
-        window->draw(map);
-        for (size_t i = 0; i != players.size(); i++) {
-            players[i].setPosition(data.players[i].position.x, data.players[i].position.y);
-            players[i].setRotation(data.players[i].position.angle);
-            window->draw(players[i]);
-        }
+    view.setCenter(sf::Vector2f(data.players[0].position.x, data.players[0].position.y));
+    window->setView(view);
+    window->draw(map);
+    for (size_t i = 0; i != players.size(); i++) {
+        players[i].setPosition(data.players[i].position.x, data.players[i].position.y);
+        players[i].setRotation(data.players[i].position.angle);
+        window->draw(players[i]);
     }
-
     window->display();
     return 0;
 }
